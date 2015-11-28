19a42a96de300b2fd7f2c3a5c146411eb037d4d8
Davis Gossage <dcg13@duke.edu>
2015-11-28 14:52:28 -0500

    Function for getting current season

--- cloud/main.js
+++ cloud/main.js
@@ -4,3 +4,17 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+Parse.Cloud.define("getCurrentSeason", function(request, response) {
+	var query = new Parse.Query("Season");
+	query.equalTo("current",true);
+	query.first({
+		success: function(result){
+			console.log(result);
+			response.success(result);
+		},
+		error: function() {
+			response.error("Unable to query current season.");
+		}
+	});
+});
