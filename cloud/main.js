583f59093aa5f05833132fbf8cab8c1fc34517bd
Davis Gossage <dcg13@duke.edu>
2015-11-29 17:55:58 -0500

    Secure user login cloud function, verified using google api

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,5 @@
+var _ = require('underscore');
+
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
@@ -18,3 +20,113 @@ Parse.Cloud.define("getCurrentSeason", function(request, response) {
 		}
 	});
 });
+
+Parse.Cloud.define("authenticatedSignin", function(request, response) {
+	Parse.Cloud.httpRequest({
+		url: 'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token='+request.params.access_token
+	}).then(function(httpResponse){
+		if (httpResponse.data.user_id){
+			//got a valid user_id, find the TokenStorage object
+			locateOrCreateUser(httpResponse.data.user_id,request.params.access_token).then(function(user){
+				console.log(user);
+				response.success(user);
+			}, function(error){
+				response.error(error);
+			});
+		}
+		else{
+			response.error("Sign in failed.  Invalid response while validating with Google.")
+		}
+	}, function(httpResponse){
+		response.error("Sign in failed.  Unable to validate token with Google.");
+	});
+});
+
+
+locateOrCreateUser = function(userId,token){
+	var promise = new Parse.Promise();
+
+	Parse.Cloud.useMasterKey();
+	var query = new Parse.Query("TokenStorage");
+	query.equalTo("userId",userId);
+	query.count().then(function(count){
+		if (count > 0){
+			userWithUserId(query,token).then(function(user){
+				promise.resolve(user);
+			}, function(error){
+				promise.reject(error);
+			});
+		}
+		else{
+			newUser(userId,token).then(function(user){
+				promise.resolve(user);
+			}, function(error){
+				promise.reject(error);
+			});
+		}
+	});
+		
+
+	return promise;
+}
+
+userWithUserId = function(userIdQuery,token){
+	var promise = new Parse.Promise();
+
+	var tokenStorageObject;
+
+	userIdQuery.first().then(function(object){
+		tokenStorageObject = object;
+		tokenStorageObject.set("accessToken",token);
+		return tokenStorageObject.save();
+	}).then(function(){
+		var query = new Parse.Query(Parse.User);
+		query.equalTo("tokenStorage",tokenStorageObject);
+		return query.first();
+	}).then(function(user){
+		promise.resolve(user);
+	}, function(error){
+		promise.reject(error)
+	});
+
+	return promise;
+}
+
+newUser = function(userId,token){
+	var promise = new Parse.Promise();
+
+	var user = new Parse.User();
+	var Buffer = require('buffer').Buffer;
+
+	//create new TokenStorage object
+	var tokenStorage = new Parse.Object("TokenStorage");
+	tokenStorage.set("accessToken",token);
+	tokenStorage.set("userId",userId);
+	//restricted ACL
+	var restrictedAcl = new Parse.ACL();
+	restrictedAcl.setPublicReadAccess(false);
+	restrictedAcl.setPublicWriteAccess(false);
+	tokenStorage.setACL(restrictedAcl);
+	
+	tokenStorage.save().then(function(){
+		//generate random username and password, as users are located using TokenStorage
+		var username = new Buffer(24);
+		var password = new Buffer(24);
+		_.times(24, function(i) {
+		  username.set(i, _.random(0, 255));
+		  password.set(i, _.random(0, 255));
+		});
+		user.set("username", username.toString('base64'));
+		user.set("password", password.toString('base64'));
+		user.set("tokenStorage",tokenStorage);
+		return user.signUp();
+	}).then(function(){
+		promise.resolve(user);
+	}, function(error){
+		tokenStorage.destroy();
+		promise.reject();
+	});
+
+	return promise;
+}
+
